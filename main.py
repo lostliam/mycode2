61160e9cce554a0e94c71dc5184818eabdb2d353
lostliam <lostliamj@outlook.com>
2023-05-09 13:48:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,52 @@
 import requests,re,os
 from random import randint
 from time import sleep
-# WEBSITE=os.getenv('WEBSITE')
-WEBSITE='https://gogoo.cyou'
+import warnings 
+warnings.filterwarnings("ignore")
+
+WEBSITE=os.getenv('WEBSITE')
 mail=str(randint(100000000,9999999999))+'@qq.com'
 
-data = '------WebKitFormBoundaryqhEFAFCsKLBfEqRY\r\nContent-Disposition: form-data; name="passwd"\r\n\r\n{}\r\n------WebKitFormBoundaryqhEFAFCsKLBfEqRY\r\nContent-Disposition: form-data; name="repasswd"\r\n\r\n{}\r\n------WebKitFormBoundaryqhEFAFCsKLBfEqRY\r\nContent-Disposition: form-data; name="geetest_code"\r\n\r\n111\r\n------WebKitFormBoundaryqhEFAFCsKLBfEqRY\r\nContent-Disposition: form-data; name="code"\r\n\r\n\r\n------WebKitFormBoundaryqhEFAFCsKLBfEqRY\r\nContent-Disposition: form-data; name="geetest_challenge"\r\n\r\n6d80c7a33d63e496e46fc5b2f4ae7dbeac\r\n------WebKitFormBoundaryqhEFAFCsKLBfEqRY\r\nContent-Disposition: form-data; name="geetest_validate"\r\n\r\naa38062903058b8013d7c27020a1107e\r\n------WebKitFormBoundaryqhEFAFCsKLBfEqRY\r\nContent-Disposition: form-data; name="geetest_seccode"\r\n\r\naa38062903058b8013d7c27020a1107e%7Cjordan\r\n------WebKitFormBoundaryqhEFAFCsKLBfEqRY\r\nContent-Disposition: form-data; name="email"\r\n\r\n{}\r\n------WebKitFormBoundaryqhEFAFCsKLBfEqRY\r\nContent-Disposition: form-data; name="name"\r\n\r\n{}\r\n------WebKitFormBoundaryqhEFAFCsKLBfEqRY--\r\n'.format(mail,mail,mail,mail)
 
 headers = {
     'accept': 'application/json, text/javascript, */*; q=0.01',
     'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
     'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'sec-ch-ua': '"Chromium";v="112", "Microsoft Edge";v="112", "Not:A-Brand";v="99"',
+    'sec-ch-ua': '"Microsoft Edge";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
     'sec-ch-ua-mobile': '?1',
     'sec-ch-ua-platform': '"Android"',
     'sec-fetch-dest': 'empty',
     'sec-fetch-mode': 'cors',
     'sec-fetch-site': 'same-origin',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36 Edg/112.0.1722.64',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Mobile Safari/537.36 Edg/113.0.1774.35',
     'x-requested-with': 'XMLHttpRequest',
 }
 
 data = {
     'email': mail,
+    'name': mail,
+    'passwd': mail,
+    'repasswd': mail,
+    'geetest_code': '111',
+    'code': '',
+    'geetest_challenge': '99de83c52927f403d186ccb7e9140964gi',
+    'geetest_validate': 'f085c7b27d9111fac536fb7e1dba5396',
+    'geetest_seccode': 'f085c7b27d9111fac536fb7e1dba5396|jordan',
+}
+
+data1 = {
+    'email': mail,
     'passwd': mail,
     'code': '',
 }
 
-data1 = {
+data2 = {
     'coupon': '',
     'shop': '1',
     'autorenew': '0',
     'disableothers': '1',
 }
-proxies={'http': '1.117.79.73:3128', 'https': '1.117.79.73:3128'}
+# proxies={'http': '127.0.0.1:7890', 'https': '127.0.0.1:7890'}
 
 def post(url,headers,cookies='',data='',proxies=''):
     attempts = 0
@@ -46,7 +59,8 @@ def post(url,headers,cookies='',data='',proxies=''):
                 response = requests.post(url,  headers=headers, data=data,proxies=proxies)
                 global CK
                 CK= response.cookies
-            #print(response.text.encode('utf-8').decode('unicode_escape'))
+            print(response.text.encode('utf-8').decode('unicode_escape'))
+            break
         except Exception as e:
             attempts += 1
             if attempts >= max_attempts:
@@ -62,10 +76,11 @@ def get(url,headers,cookies='',proxies=''):
         try:
             response =requests.get(url,cookies=cookies,headers=headers,proxies=proxies)
             url=re.search(r'https://(.*?)clash=1&extend=1',response.text).group()
-            #print(response.text.encode('utf-8').decode('unicode_escape'))
+            print(response.text.encode('utf-8').decode('unicode_escape'))
             response = requests.get(url, headers=headers,proxies=proxies)
             with open('example.txt', 'w') as f:
                 f.write(response.text)
+            break
         except Exception as e:
             attempts += 1
             if attempts >= max_attempts:
@@ -76,8 +91,8 @@ def get(url,headers,cookies='',proxies=''):
 
 
 post(WEBSITE+'/auth/register',  headers=headers, data=data,proxies=proxies)
-post(WEBSITE+'/auth/login',  headers=headers, data=data,proxies=proxies)
-post(WEBSITE+'/user/buy', cookies=CK, headers=headers, data=data1,proxies=proxies)
+post(WEBSITE+'/auth/login',  headers=headers, data=data1,proxies=proxies)
+post(WEBSITE+'/user/buy', cookies=CK, headers=headers, data=data2,proxies=proxies)
 post(WEBSITE+'/user/checkin', cookies=CK, headers=headers,proxies=proxies)
 get(WEBSITE+'/user', cookies=CK, headers=headers,proxies=proxies)
 
